4e879e14d9afaf527e7e154e379f3b4a0a7def03
Fabio Lorenzi <fabiolorenzi13975@gmail.com>
2025-08-26 08:04:06 +0100

    changed icon

--- src/main.cpp
+++ src/main.cpp
@@ -1,8 +1,10 @@
 #include <QApplication>
+#include <QIcon>
 #include "classes/MainWindow.h"
 
 int main(int argc, char* argv[]) {
     QApplication app(argc, argv);
+    app.setWindowIcon(QIcon(":/icons/icon.png"));
 
     MainWindow window;
     window.show();
